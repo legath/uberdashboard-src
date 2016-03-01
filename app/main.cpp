c6d83fd62a41099cf6a4cb63267ca58b86f81c51
Alexander Bulychev <a.f.bulychev@gmail.com>
2016-03-01 18:47:29 +0300

    added empty main

---
+++ app/main.cpp
@@ -0,0 +1,22 @@
+//
+// This file is part of the Uberdashboard project
+// Copyright (c) 2016 Alexander Bulychev (a.f.bulychev@gmail.com).
+//
+//
+// ----------------------------------------------------------------------------
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void main(void)
+{
+  
+  while (1)
+    {
+       
+    }
+}
+
+
+
+// ----------------------------------------------------------------------------

0a9e14dda4d34556149856ee918ae8f6040c2d12
Alexander Bulychev <a.f.bulychev@gmail.com>
2016-03-10 19:20:04 +0300

    first task template for compilation test

--- app/main.cpp
+++ app/main.cpp
@@ -7,16 +7,35 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include "stm32f4xx.h"
+#include <scmRTOS.h>
+
+typedef OS::process<OS::pr0, 300> TProc0;
+
+TProc0 Proc0;
 
 int main(void)
 {
+	
+	OS::run();
 
-  while (1)
+}
+namespace OS
+{
+    template <>
+    OS_PROCESS void TProc0::exec()
     {
-
+        for(;;)
+        {
+        	
+        }
     }
 }
 
 
+extern "C" uint32_t HAL_GetTick(void)
+{
+  return OS::get_tick_count();
+}
 
 // ----------------------------------------------------------------------------
